58ec87246240a211571ed17e5753eafa113daaf4
bojorquezja <bojorquez.ja@gmail.com>
2025-06-27 19:30:57 -0500

    a medio hacer PD

--- cantcaminos.py
+++ cantcaminos.py
@@ -7,6 +7,15 @@ lab=[[1,1,1,1,1,1,1,1],
      [1,0,1,1,1,0,1,1],
      [1,1,1,1,1,1,1,1]]
 
+mem=[[-1]*len(lab[0]) for _ in range(len(lab))]
+
+def imprime(mat):
+    for f in mat:
+        for c in f:
+            print(f"{c},",end="")
+        print()
+    print()
+
 def caminosR(lab, f,c):
     global op
     op += 1
@@ -30,7 +39,34 @@ def caminosR(lab, f,c):
             return caminosR(lab, f,c+1)+caminosR(lab, f+1,c)
         else:
             return 0
+
+def caminosPD(lab, mem, f,c):
+    global op
+    op += 1
+    if f == len(lab)-1 and c == len(lab[0])-1 :
+        if lab[f][c] == 1:
+            return 1
+        else:
+            return 0
+    elif c == len(lab[0])-1:
+        if lab[f][c] == 1:
+            return caminosPD(lab, f+1,c)
+        else:
+            return 0
+    elif f == len(lab)-1:
+        if lab[f][c] == 1:
+            return caminosPD(lab, f,c+1)
+        else:
+            return 0
+    else:
+        if lab[f][c] == 1:
+            return caminosPD(lab, f,c+1)+caminosPD(lab, f+1,c)
+        else:
+            return 0
         
         
 op=0
-print(f"{caminos(lab, 0,0)} caminos con {op} operaciones")
+print(f"{caminosR(lab, 0,0)} caminos con {op} operaciones")
+op=0
+print(f"{caminosPD(lab, mem, 0,0)} caminos con {op} operaciones")
+
